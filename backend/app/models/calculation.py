4797b10c74b9a17ab8b279f78f03e393b3067a25
salman-bokhari <salman.bokhari@eurustechnologies.com>
2025-12-08 04:27:09 +0500

    Testing

--- backend/app/models/calculation.py
+++ backend/app/models/calculation.py
@@ -1,17 +1,15 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from backend.app.routers.auth import get_current_user
+from backend.app.database import Base
 
 class Calculation(Base):
     __tablename__ = "calculations"
 
     id = Column(Integer, primary_key=True, index=True)
     operation = Column(String, nullable=False)
-    operand1 = Column(Float, nullable=False)
-    operand2 = Column(Float, nullable=False)
-    result = Column(Float, nullable=False)
+    operand1 = Column(Integer, nullable=False)
+    operand2 = Column(Integer, nullable=False)
+    result = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
-    user_id = Column(Integer, ForeignKey("users.id"))
-
-    # FIX: reference User by string
-    user = relationship("User", back_populates="calculations")
+    user = relationship("User")
